6d3ac48d046a4ad647784eeac9f7f83c2939ae1d
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-18 17:31:40 -0400

    file added

---
+++ src/echoesIndex.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+
+/******************************** Constant variables ********************************/
+const time = 300 //game lasts for 300 seconds
+const lifeBar =  [];
+const timeRemaining = [];
+const roomImagesArray = []; 
+const enemyImages = [];
+const notesImages = [];
+
+/************  Html views  ************/
+  // View divs
+const landingPage = document.querySelector("#landingPage");
+
+/******************************** Data elements ********************************/
+// rooms
+cuarters.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/cuarters.jpg?raw=true";
+gameOverImage.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/gameOver.jpg?raw=true";
+gameWonImage.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/gameWon.png?raw=true";
+hallRoom.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/hall.jpg?raw=true";
+laboratoryPristine.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/lab1.jpg?raw=true";
+laboratoryDestroyed.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/lab2.jpg?raw=true";
+entranceRoom.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/vault.jpg?raw=true";
+orbRoom.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/orbRoom.jpg?raw=true";
+cipherfail.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/alternateLab.jpg?raw=true";
+landingPage.src = "https://prompthero.com/rails/active_storage/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaEpJaWt3TlRBNFlqZzJOUzA1WVdJMkxUUXlZV1V0T1dZME1pMHdNR1F3TmprME4ySXhPREFHT2daRlZBPT0iLCJleHAiOm51bGwsInB1ciI6ImJsb2JfaWQifX0=--bd34b9e9ce5bc68d800eec32e7ae9ab7daebcc2f/prompthero-prompt-58d830c1fbd.png";
+detective.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/detective.png?raw=true";
+
+// enemies
+eyesMob.src= "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/eyes.png?raw=true";
+mob1.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/mob1.png?raw=true";
+boss.src = "https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/mob2.png?raw=true";
+
+// notes
+note1.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/note1.jpg?raw=truehttps://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/note1.jpg?raw=true";
+note2.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/note2.jpg?raw=true";
+note3.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/note3.jpg?raw=true";
+note4.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/note4.jpg?raw=true";
+plaque.src ="https://github.com/lerodriguezreyes/m1p-echoes/blob/main/Images/plaque.jpg?raw=true";
+
+// narrative bits
+
+
+/******************************** Timer ********************************/
+let timer;
+
+function gameTime() {
+
+}
+
+// Enable music and loop the track
+let vid = document.getElementById("music");
+vid.preload = "auto";
+vid.loop = true;
+
+
+})
