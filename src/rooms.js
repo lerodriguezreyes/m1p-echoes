fb478969e028c0f317740d831904bedf652bc756
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-19 16:56:29 -0400

    Constructure refractor

--- src/rooms.js
+++ src/rooms.js
@@ -1,40 +1,18 @@
 class Narrative {
-    constructor(introduction, decision, choice, room){
-        this.introduction = introduction;
-        this.decision = decision;
-        this.choice = choice;
-        this.room = room;
-    }
     
-    constructor (roomImage,lifeRemaining,timeLimit,timeRemaining, narrativeContent, choice, notes,){
-
-    }
-}
-getNotes(){
-    return this.notes[this.currentNotesIndex];
-}
-
-shuffleNotes() {
-    for (let i = this.notes.length - 1; i > 0; i--) { 
-          const j = Math.floor(Math.random() * (i + 1)); 
-          [this.notes[i], this.notes[j]] = [this.notes[j], this.notes[i]]; 
-        } 
-        return this.notes; 
+    constructor(roomImage,roomDescription, roomChoices, notes){
+        this.roomImage = roomImage;
+        this.roomDescription = roomDescription;
+        this.roomChoices = roomChoices;
+        this.roomImage = roomImage;
+        this.notes = notes;
     }
 
-
-        /* choices and checks yet to be programed
-orb room:
-
-if(choice)
-
-
-
-
-
-
-
-
-
-        */
-    
+    // shuffleNotes() {
+    //     for (let i = this.notes.length - 1; i > 0; i--) { 
+    //         const j = Math.floor(Math.random() * (i + 1)); 
+    //         [this.notes[i], this.notes[j]] = [this.notes[j], this.notes[i]]; 
+    //       } 
+    //       return this.choices;
+    // }
+}
